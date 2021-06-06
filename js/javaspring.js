a4fef7792603008044d0f047be4369fffcffe7f9
TRUONGBB\truon <truongbb96@outlook.com>
2021-06-06 18:32:13 +0700

    fix: edit

--- js/javaspring.js
+++ js/javaspring.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    let id = 1;
+    let id = 0;
     let list = [];
     let isEdit = false;
     let index = -1
 
     $("#save-button").click(function () {
-        var no = id;
-        var name = $("#name").val();
-        var address = $("#address").val();
-        var phoneNumber = $("#phoneNumber").val();
-        var className = $("#className").val();
+        const name = $("#name").val();
+        const address = $("#address").val();
+        const phoneNumber = $("#phoneNumber").val();
+        const className = $("#className").val();
         if (name == "" || address == "" || phoneNumber == "" || className == "") {
             alert("Nhập thiếu hoặc nhập sai! vui lòng kiểm tra lại");
             return 0;
@@ -17,62 +16,57 @@ $(document).ready(function () {
 
         if (isEdit) {
             doUpdateInfo(name, address, phoneNumber, className);
-            $("#name").val(name);
-            $("#address").val(address);
-            $("#phoneNumber").val(phoneNumber);
-            $("#className").val(className);
             isEdit = false;
             return;
         }
 
         $("#table-data").append("<tr id='" + id + "'>" +
             "<td><button type='button' class='delete'>Delete</button><button type='button' class='edit'>Edit</button></td>" +
-            "<td class='no'>" + no + "</td>" +
+            "<td class='no' style='display: none'>" + id + "</td>" +
             "<td class='name'>" + name + "</td>" +
             "<td class='address'>" + address + "</td>" +
             "<td class='phoneNumber'>" + phoneNumber + "</td>" +
             "<td class='className'>" + className + "</td>" +
             "</tr>");
-        var ob = { no, name, address, phoneNumber, className };
+        const ob = { id, name, address, phoneNumber, className };
         list.push(ob);
         clearForm();
         id++;
     });
 
     $('table').on('click', '.delete', function () {
-        $(this).parents('tr').remove();
+        const rowData = $(this).parents('tr');
+        index = rowData.children('.no').text();
+        rowData.remove();
+        list.splice(index, 1);
     });
 
     $('table').on('click', '.edit', function () {
-        const no = $(this).parents('tr').children('.no').text();
-        index = searchIndexById(no);
-        console.log(index);
-        const name = $(this).parents('tr').children('.name').text();
-        const address = $(this).parents('tr').children('.address').text();
-        const phoneNumber = $(this).parents('tr').children('.phoneNumber').text();
-        const className = $(this).parents('tr').children('.className').text();
+        const rowData = $(this).parents('tr');
+        index = parseInt(rowData.children('.no').text());
 
-        document.getElementById('name').value = name;
-        document.getElementById('address').value = address;
-        document.getElementById('phoneNumber').value = phoneNumber;
-        document.getElementById('className').value = className;
+        document.getElementById('name').value = rowData.children('.name').text();
+        document.getElementById('address').value = rowData.children('.address').text();
+        document.getElementById('phoneNumber').value = rowData.children('.phoneNumber').text();
+        document.getElementById('className').value = rowData.children('.className').text();
 
         isEdit = true;
     });
-
-    function searchIndexById(tmp) {
-        for (var i = 0; i < list.length; i++) {
-            if (list[i].no == tmp) return i;
-        }
-        return -1;
-    }
 
     function doUpdateInfo(name, address, phoneNumber, className) {
         list[index].name = name;
         list[index].address = address;
         list[index].phoneNumber = phoneNumber;
         list[index].className = className;
+
+        const rowData = $('table').find('tr#' + index);
+        rowData.children('.name').text(name);
+        rowData.children('.address').text(address);
+        rowData.children('.phoneNumber').text(phoneNumber);
+        rowData.children('.className').text(className);
+
         index = -1;
+        clearForm();
     }
 
     function clearForm() {
@@ -81,4 +75,5 @@ $(document).ready(function () {
         document.getElementById("phoneNumber").value = "";
         document.getElementById("className").value = "";
     }
+
 });
